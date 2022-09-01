e8658907cbd1b2e7c340561419d9d877c274591c
Shruti <sdnk1993@gmail.com>
2022-09-01 20:41:10 +0530

    commit in UC2

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,15 +1,21 @@
 public class EmployeeWage {
+    public static int total_wage_hour = 0;
+    public static int wage_per_hour=20;
+    public static int working_hour=8;
     public static int randomval() {
         int empcheck = (int) (Math.random() * 10 % 2);
         return empcheck;
     }
+        public static int calc() {
+        int isPresent=randomval();
+            if (isPresent == 1) {
+                total_wage_hour = working_hour * wage_per_hour;
+            }
+            return total_wage_hour;
+        }
+
     public static void main(String[] args) {
         EmployeeWage emp = new EmployeeWage();
-        int chk = emp.randomval();
-        if (chk == 0) {
-            System.out.println("Employee is present");
-        } else {
-            System.out.println("Employee is absent");
-        }
+        System.out.println("Total Employee wage is :"+emp.calc());
     }
 }
