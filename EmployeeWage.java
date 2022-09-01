bc10c08b1baf3952538eb74ebbac4d23d08e5cb1
Shruti <sdnk1993@gmail.com>
2022-09-01 20:08:12 +0530

    commit in UC1

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,9 +1,15 @@
 public class EmployeeWage {
-    public static void randomval() {
-        System.out.println("Welcome to Employee Wages Computation");
+    public static int randomval() {
+        int empcheck = (int) (Math.random() * 10 % 2);
+        return empcheck;
     }
     public static void main(String[] args) {
         EmployeeWage emp = new EmployeeWage();
-        emp.randomval();
+        int chk = emp.randomval();
+        if (chk == 0) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
