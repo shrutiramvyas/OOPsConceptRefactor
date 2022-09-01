adecba9230e9b3fd5637104d95790df9df052064
Shruti <sdnk1993@gmail.com>
2022-09-01 20:06:46 +0530

    firt commit

---
+++ EmployeeWage.java
@@ -0,0 +1,9 @@
+public class EmployeeWage {
+    public static void randomval() {
+        System.out.println("Welcome to Employee Wages Computation");
+    }
+    public static void main(String[] args) {
+        EmployeeWage emp = new EmployeeWage();
+        emp.randomval();
+    }
+}
