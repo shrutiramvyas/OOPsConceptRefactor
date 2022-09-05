7933a17fc6c1ceacb7d07ed58bd5ae4240ec01a8
Shruti <sdnk1993@gmail.com>
2022-09-05 18:09:54 +0530

    fifth commit

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,33 +1,33 @@
 class ClassEmployee {
-    public static final int full_time=2;
-    public static final int part_time=1;
-    public int emp_rate_per_hour;
-    public int emphrs=0;
-    public int empwage=0;
-    ClassEmployee(int wage_per_hour){
-        this.emp_rate_per_hour=wage_per_hour;
-    }
-    public int calc() {
-        int empcheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empcheck) {
-            case part_time:
-                emphrs = 4;
-                break;
-            case full_time:
-                emphrs = 8;
-                break;
-            default:
-                emphrs = 0;
-        }
-        empwage = emphrs * emp_rate_per_hour;
-        return empwage;
+    public static final int part_time = 1;
+    public static final int full_time = 2;
+    public static final int emp_rate_per_hour = 20;
+    public static final int no_of_working_days = 20;
 
+    public int calc() {
+        int emp_hours = 0, total_working_days = 0;
+        while (total_working_days < no_of_working_days) {
+            total_working_days++;
+            int empcheck = (int) Math.floor(Math.random() * 10 % 3);
+            switch (empcheck) {
+                case part_time:
+                    emp_hours = 4;
+                    break;
+                case full_time:
+                    emp_hours = 8;
+                    break;
+                default:
+                    emp_hours = 0;
+            }
+        }
+        int totalEmpWages = emp_hours * emp_rate_per_hour;
+        return totalEmpWages;
     }
 }
+
 class Main{
     public static void main(String[] args) {
-        int wage_per_hour=20;
-        ClassEmployee emp =new ClassEmployee(wage_per_hour);
+        ClassEmployee emp =new ClassEmployee();
         System.out.println("Total wages of employee is: " + emp.calc());
 
     }
