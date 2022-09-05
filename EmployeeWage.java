4c15df441bf17d1536b4f58dfaa35cae053f3990
Shruti <sdnk1993@gmail.com>
2022-09-05 18:08:31 +0530

    fourth commit

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,34 +1,34 @@
-public class EmployeeWage {
-    public int full_time=2;
-    public int part_time=1;
-    public static int total_wage_hour = 0;
-    public static int wage_per_hour=20;
-    public static int working_hour=8;
+class ClassEmployee {
+    public static final int full_time=2;
+    public static final int part_time=1;
+    public int emp_rate_per_hour;
     public int emphrs=0;
     public int empwage=0;
-    public int emp_rate_per_hour=20;
-    public static int randomval() {
-        int empcheck = (int) (Math.random() * 10 % 2);
-        return empcheck;
+    ClassEmployee(int wage_per_hour){
+        this.emp_rate_per_hour=wage_per_hour;
     }
-    public int calc(){
-        double empcheck =Math.floor(Math.random()*10)%3;
-        switch(empcheck) {
-            case 1:
+    public int calc() {
+        int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empcheck) {
+            case part_time:
                 emphrs = 4;
                 break;
-            case 2:
+            case full_time:
                 emphrs = 8;
                 break;
             default:
-                emphrs=0;
+                emphrs = 0;
         }
-        empwage=emphrs*emp_rate_per_hour;
+        empwage = emphrs * emp_rate_per_hour;
         return empwage;
-    }
 
+    }
+}
+class Main{
     public static void main(String[] args) {
-        EmployeeWage emp = new EmployeeWage();
-        System.out.println("Total Employee wage is :"+emp.calc());
+        int wage_per_hour=20;
+        ClassEmployee emp =new ClassEmployee(wage_per_hour);
+        System.out.println("Total wages of employee is: " + emp.calc());
+
     }
 }
