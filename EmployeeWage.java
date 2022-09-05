dd5571c8c83f8d4aceaa699bb28bd72856cad31e
Shruti <sdnk1993@gmail.com>
2022-09-05 18:10:58 +0530

    sixth commit

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,10 +3,10 @@ class ClassEmployee {
     public static final int full_time = 2;
     public static final int emp_rate_per_hour = 20;
     public static final int no_of_working_days = 20;
-
-    public int calc() {
-        int emp_hours = 0, total_working_days = 0;
-        while (total_working_days < no_of_working_days) {
+    public static final int max_hour_in_month = 10;
+    public int calc(){
+        int emp_hours = 0, total_emp_hrs = 0, total_working_days = 0;
+        while(total_emp_hrs <= max_hour_in_month && total_working_days < no_of_working_days) {
             total_working_days++;
             int empcheck = (int) Math.floor(Math.random() * 10 % 3);
             switch (empcheck) {
@@ -19,8 +19,10 @@ class ClassEmployee {
                 default:
                     emp_hours = 0;
             }
+            total_emp_hrs += emp_hours;
+            System.out.println("Day" + total_working_days + " Emp Hr: " + emp_hours);
         }
-        int totalEmpWages = emp_hours * emp_rate_per_hour;
+        int totalEmpWages=total_emp_hrs*emp_rate_per_hour;
         return totalEmpWages;
     }
 }
