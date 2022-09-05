347387b4bc562b9cdcc6a6e0a6ef3c54e63e0bee
Shruti <sdnk1993@gmail.com>
2022-09-05 17:46:31 +0530

    Third commit

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,18 +1,30 @@
 public class EmployeeWage {
+    public int full_time=2;
+    public int part_time=1;
     public static int total_wage_hour = 0;
     public static int wage_per_hour=20;
     public static int working_hour=8;
+    public int emphrs=0;
+    public int empwage=0;
+    public int emp_rate_per_hour=20;
     public static int randomval() {
         int empcheck = (int) (Math.random() * 10 % 2);
         return empcheck;
     }
-        public static int calc() {
-        int isPresent=randomval();
-            if (isPresent == 1) {
-                total_wage_hour = working_hour * wage_per_hour;
-            }
-            return total_wage_hour;
+    public int calc(){
+        double empcheck =Math.floor(Math.random()*10)%3;
+        if(empcheck==part_time) {
+            emphrs = 4;
         }
+        else if(empcheck==full_time) {
+            emphrs = 8;
+        }
+        else{
+            emphrs=0;
+        }
+        empwage=emphrs*emp_rate_per_hour;
+        return empwage;
+    }
 
     public static void main(String[] args) {
         EmployeeWage emp = new EmployeeWage();
